8e06663e67ee456dd18af5046bfb88f3d960db1e
Ankita Gupta <49222160+ankita219819@users.noreply.github.com>
2020-01-03 11:56:48 +0530

    Add files via upload

--- Web-Application/register.js
+++ Web-Application/register.js
@@ -50,7 +50,6 @@ function reset()
     document.getElementById("registerTemplate").reset();
 }
 function validBirth(){
-
     var dob=new Date(document.getElementById('dob').value);
     var currentDate=new Date();
     if(currentDate.getFullYear()-dob.getFullYear()<18){
@@ -59,4 +58,29 @@ function validBirth(){
     else{
         document.getElementById('dobMsg').innerHTML="";
     }
+}
+function years(){
+   var currentDate=new Date();
+   var current=(currentDate.getFullYear()-18);
+   console.log(current);
+   newYear=currentDate.setFullYear(current);
+   console.log(newYear);
+   var newD=new Date(newYear);
+   console.log(newYear);
+   var thisdate=newD.toString();
+   var nowDate=convertDate(thisdate);
+   //console.log(convertDate(thisdate))
+   //new Date(newYear);
+   alert(newD)
+   document.getElementById('dob').setAttribute("max",nowDate);
+   
+}
+
+function convertDate(strDate){
+    console.log(strDate);
+    var date = new Date(strDate),
+    mnth = ("0" + (date.getMonth() + 1)).slice(-2),
+    day = ("0" + date.getDate()).slice(-2);
+  return [date.getFullYear(), mnth, day].join("-");
+    //return strDate;
 }
